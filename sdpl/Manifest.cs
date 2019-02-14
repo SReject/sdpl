55ec9ab9edadc2bd114469ded06b1e4bd35e7ad4
SReject <sreject@comcast.net>
2019-02-14 07:09:07 -0500

    Initial Commit

---
+++ sdpl/Manifest.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace sdpl {
+    public class SDManifest {
+        public Manifest SDPL { get; private set; }
+    }
+    public class Manifest {
+        public bool UseShell { get; set; } = false;
+        public bool ConsoleApp { get; set; } = false; // Not implemented yet;
+        public bool HideWindow { get; set; } = true;
+        public string Path { get; set; } = "";
+        public string Arguments { get; set; } = "";
+
+        public static Manifest Load(string[] args) {
+            Logger.Log("[Manifest] Loading");
+
+            // Deduce path to manifest.json
+            string manifestPath = System.IO.Path.Combine(Plugin.Directory, "manifest.json");
+
+            // variable declares
+            string manifestText;
+            SDManifest sdmanifest;
+
+            // Read the contents of manifest.json
+            try {
+
+                Logger.Log($"[Manifest] Attempting to read: {manifestPath}");
+                manifestText = File.ReadAllText(manifestPath);
+
+            } catch (Exception ex) {
+                throw new Exception($"[Manifest] Failed to load: {ex.Message}");
+            }
+
+            // Deserialize the contents of manifest.json
+            try {
+                Logger.Log("[Manifest] Parsing manifest");
+                sdmanifest = JsonConvert.DeserializeObject<SDManifest>(manifestText);
+
+            } catch (Exception ex) {
+                throw new Exception($"[Manifest] Could not process manifest: {ex.Message}");
+            }
+
+            // Extract manifest.json's SDPL entry
+            if (sdmanifest.SDPL == null) {
+                throw new Exception("[Manifest] SDPL property missing or invalid");
+            }
+            Manifest manifest = sdmanifest.SDPL;
+
+            // concat arguments listed in manifest with args passed into main
+            if (args.Length > 0) {
+                if (manifest.Arguments != "") {
+                    manifest.Arguments += " ";
+                }
+                manifest.Arguments += String.Join(" ", args).Replace("\"", "\"\"\"");
+            }
+
+            // return the manifest
+            Logger.Log("[Manifest] Ready");
+            return manifest;
+        }
+    }
+}
---
+++ sdpl/Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace sdpl {
+    class Plugin {
+        public static string Directory = System.IO.Directory.GetCurrentDirectory();
+        public static Launch launcher;
+
+        private static bool LauncherRunning = false;
+
+        static void Main(string[] args) {
+
+            // Initialize the logger
+            Logger.Init();
+            Logger.Log($"[Main] Starting in: {Directory}");
+
+            // listen for exit events
+            AppDomain.CurrentDomain.ProcessExit += AppExitHandler;
+            Console.CancelKeyPress += AppExitHandler;
+
+            // Attemt to load manifest.json
+            Manifest manifest;
+            try {
+                manifest = Manifest.Load(args);
+
+            } catch (Exception ex) {
+                Logger.Log(ex.Message);
+                return;
+            }
+
+            // attempt to start the launcher
+            try {
+                launcher = new Launch(manifest);
+                LauncherRunning = true;
+                launcher.ProcessExited += Launch_ProcessExited;
+
+            } catch (Exception ex) {
+                Logger.Log($"[Launcher] {ex.Message}");
+            }
+        }
+
+        // Plugin received a close 'message'
+        private static void AppExitHandler(object sender, EventArgs evt) {
+            Logger.Log("[Main] Exiting");
+
+            // launched process still running; attempt to exit
+            if (LauncherRunning) {
+                Logger.Log("[Process] Exiting");
+                LauncherRunning = false;
+                launcher.Close();
+            }
+        }
+
+        // launched process exited
+        private static void Launch_ProcessExited(object sender, Launch.ProcessExitedArgs e) {
+            LauncherRunning = false;
+            Logger.Log("[Process] Exited");
+        }
+    }
+}
